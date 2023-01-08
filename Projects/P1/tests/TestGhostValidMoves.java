ee7004ef06ce22e46f6a223f16b1916f934cacfe
whe1123 <whe@Xiao-2020.localdomain>
2023-01-07 19:57:12 -0500

    changed tests in FTR-ghost-get_valid_moves.

--- Projects/P1/tests/TestGhostValidMoves.java
+++ Projects/P1/tests/TestGhostValidMoves.java
@@ -1,9 +1,33 @@
 import java.io.*;
+import java.util.ArrayList;
+
 import junit.framework.*;
+import java.awt.Color;
 
 public class TestGhostValidMoves extends TestCase {
 
   public void testGhostValidMoves() throws FileNotFoundException {
-    return null;
+    NoFrame frame = new NoFrame();
+
+    //tests that ghost can move through pacman even if trapped.
+    Ghost ghost = frame.addGhost(new Location(1, 1), "Joe", Color.red);
+    PacMan pacman2 = frame.addPacMan(new Location(2, 1));
+    PacMan pacman = frame.addPacMan(new Location(1, 2));
+    
+
+    ArrayList<Location> allowedMoves = new ArrayList<Location>();
+    allowedMoves.add(new Location(2, 1));
+    allowedMoves.add(new Location(1, 2));
+
+    assertEquals(ghost.get_valid_moves(),(allowedMoves));
+    System.out.println("Passed ghost being trapped by pacman.");
+
+
+    NoFrame frame2 = new NoFrame();
+
+    Ghost ghost2 = frame2.addGhost(new Location(500, 500), "Joe", Color.red);
+    assertTrue(ghost2.get_valid_moves().isEmpty());
+    System.out.println("Passed ghost in invalid location.");
+  
   }
 }
