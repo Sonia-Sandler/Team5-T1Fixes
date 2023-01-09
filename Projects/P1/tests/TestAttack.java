635fed141ebf72cea34a06816eb0036feaeb5f78
Justin_Pratama <jpratama@terpmail.umd.edu>
2023-01-09 14:36:42 -0500

    made changes to Ghost.java,Map.java,TestAttack.java

--- Projects/P1/tests/TestAttack.java
+++ Projects/P1/tests/TestAttack.java
@@ -14,23 +14,6 @@ public class TestAttack extends TestCase {
     assertTrue(ghost.attack());
     System.out.println("Passed ghost right attack");
 
-    NoFrame frame = new NoFrame();
-
-    Ghost ghost = frame.addGhost(new Location(1, 1), "casper", Color.red);
-    PacMan pacman = frame.addPacMan(new Location(2, 1));
-    boolean check = true;
-
-    assertTrue(ghost.attack());
-    System.out.println("Passed ghost right attack");
-
-    NoFrame frame = new NoFrame();
-
-    Ghost ghost = frame.addGhost(new Location(1, 1), "casper", Color.red);
-    PacMan pacman = frame.addPacMan(new Location(2, 1));
-
-    assertTrue(ghost.attack());
-    System.out.println("Passed ghost right attack");
-
     NoFrame frame2 = new NoFrame();
 
     Ghost ghost2 = frame2.addGhost(new Location(3, 1), "casper", Color.red);
