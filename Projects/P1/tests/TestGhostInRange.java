904c4d267b08afd0c7614fab7d59f07db4876d1f
Aman Thanvi <aman_thanvi@outlook.com>
2023-01-10 22:27:18 -0500

    Added test for pacman-ghost_in_range

--- Projects/P1/tests/TestGhostInRange.java
+++ Projects/P1/tests/TestGhostInRange.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.io.*;
 import junit.framework.*;
 
@@ -7,6 +8,14 @@ public class TestGhostInRange extends TestCase {
    * This method tests the is_ghost_in_range() method in PacMan.java
    */
   public void testGhostInRange() throws FileNotFoundException {
+    // Creating A Map
+    NoFrame frame = new NoFrame(); // Creates A New Map With Walls and Tokens w/o a Display
+    Map map = frame.getMap(); // Get the map
+    // Creating Players
+    Ghost ghost = frame.addGhost(new Location(5, 5), "name", Color.red); // Creates a red ghost named "name" at location
+    // x,y
+    PacMan pacman = frame.addPacMan(new Location(4, 4)); // Creates PacMan at location x, y
 
+    assertTrue(pacman.is_ghost_in_range()); // Test if ghost is in range
   }
 }
