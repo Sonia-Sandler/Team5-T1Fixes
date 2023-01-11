169c440bb7b1b0aba02dbd392c3aa0c257d44844
anirudhauppugunduri <anuppugunduri@gmail.com>
2023-01-10 19:01:12 -0500

    Committing and pushing TestGhostMove

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -1,7 +1,19 @@
 import java.io.*;
 import junit.framework.*;
+import java.awt.Color;
 
 public class TestGhostMove extends TestCase {
 
-  public void testGhostMove() throws FileNotFoundException {}
+  public void testGhostMove() throws FileNotFoundException {
+	  NoFrame frame = new NoFrame();
+	  
+	  // Tests that pacman's location is changing when trapped by pacmans
+	  Ghost ghost = frame.addGhost(new Location(1, 1), "Blinky", Color.red);
+	  PacMan pacman = frame.addPacMan(new Location(2, 1));
+	  PacMan pacman2 = frame.addPacMan(new Location(1, 2));
+	  
+	  Location loc = new Location(1, 1);
+	  ghost.move();
+	  assertFalse(ghost.myLoc.x == loc.x && ghost.myLoc.y == loc.y);
+  }
 }
