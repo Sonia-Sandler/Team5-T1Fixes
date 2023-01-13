4ae538062236fbb929b4b69b221df98e396b88dd
whe1123 <121910361+whe1123@users.noreply.github.com>
2023-01-12 19:00:42 -0500

    Update TestMapAttack.java

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -10,19 +10,13 @@ public class TestMapAttack extends TestCase {
     NoFrame frame = new NoFrame(); // Creates A New Map With Walls and Tokens w/o a Display
     Map map = frame.getMap(); // Gets the Map You Just Created
     // Creating Players
-    Ghost ghost = frame.addGhost(new Location(0, 0), "name", Color.red); // Creates a red ghost named "name" at location
+    
+    
+    Ghost ghost = frame.addGhost(new Location(1, 3), "name", Color.red); // Creates a red ghost named "name" at location
                                                                          // x,y
-    PacMan pacman = frame.addPacMan(new Location(1, 1)); // Creates PacMan at location x, y
+    PacMan pacman = frame.addPacMan(new Location(1, 3)); // Creates PacMan at location x, y
 
-    // alternatively if you don't need the PacMan or Ghost objects in your tests
-    frame.initPlayers(); // Creates all of the players
-
-    // Start The Game
-    frame.startGame();
-    if (map.attack(ghost.myName)) {
-      System.out.println("Pacman was attacked. Game Over!");
-    } else {
-      System.out.println("Pacman was not attacked. Game not over!");
-    }
+   
+    assertTrue(map.attack(ghost.myName));
   }
 }
